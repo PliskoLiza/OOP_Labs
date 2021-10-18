02c92943979c9100c2e71a232fd6714a15443c88
Plisko_Liza <pliskoliza@gmail.com>
2021-10-18 16:42:01 +0300

    First step

--- Lab2/src/test/java/oop/lab2/StringCalculatorTest.java
+++ Lab2/src/test/java/oop/lab2/StringCalculatorTest.java
@@ -1,4 +1,15 @@
 package oop.lab2;
 
-public class StringCalculatorTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringCalculatorTest
+{
+    @Test
+    public void Test1()
+    {
+        Assert.assertEquals(3, StringCalculator.Add("1,2"));
+        Assert.assertEquals(1, StringCalculator.Add("1"));
+        Assert.assertEquals(0, StringCalculator.Add(""));
+    }
 }
