fa4b41cf3561dc92406d4c682eef233a81af86da
Plisko_Liza <pliskoliza@gmail.com>
2021-10-18 16:47:37 +0300

    Second step

--- Lab2/src/main/java/oop/lab2/StringCalculator.java
+++ Lab2/src/main/java/oop/lab2/StringCalculator.java
@@ -5,22 +5,17 @@ public class StringCalculator
 {
     public static int Add(String numbers)
     {
-        if(numbers.length() == 0) return 0;
-        else
+        int sum = 0;
+        StringBuilder number = new StringBuilder();
+        if (numbers.length() != 0)
         {
-            int delimiters_count = 0;
-            int delimiter_place = 0;
             for (int i = 0; i < numbers.length(); i++)
             {
-                if (numbers.charAt(i) == ',') {delimiters_count ++; delimiter_place = i;}
+                if (numbers.charAt(i) != ',') {number.append(numbers.charAt(i));}
+                else {sum += Integer.parseInt(number.toString()); number.delete(0, number.length());}
             }
-            if (delimiters_count != 0 && delimiter_place != 0)
-            {
-                int first = Integer.parseInt(numbers.substring(0, delimiter_place));
-                int second = Integer.parseInt(numbers.substring(delimiter_place + 1));
-                return first + second;
-            }
-            else return Integer.parseInt(numbers);
+            sum += Integer.parseInt(number.toString());
         }
+        return sum;
     }
 }
--- Lab2/src/test/java/oop/lab2/StringCalculatorTest.java
+++ Lab2/src/test/java/oop/lab2/StringCalculatorTest.java
@@ -12,4 +12,9 @@ public class StringCalculatorTest
         Assert.assertEquals(1, StringCalculator.Add("1"));
         Assert.assertEquals(0, StringCalculator.Add(""));
     }
+    @Test
+    public void Test2()
+    {
+        Assert.assertEquals(154, StringCalculator.Add("123,2,4,5,20"));
+    }
 }
