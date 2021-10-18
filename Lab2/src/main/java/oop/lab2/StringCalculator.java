3f82c470f8de608fdefa14be8cb12c2c92dc2125
Plisko_Liza <pliskoliza@gmail.com>
2021-10-18 17:25:54 +0300

    Fifth step

--- Lab2/src/main/java/oop/lab2/StringCalculator.java
+++ Lab2/src/main/java/oop/lab2/StringCalculator.java
@@ -2,6 +2,7 @@ package oop.lab2;
 
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class StringCalculator
 {
@@ -9,27 +10,44 @@ public class StringCalculator
     {
         int sum = 0, i = 0;
         StringBuilder number = new StringBuilder();
-        ArrayList<Character> delimiters = new ArrayList<>();
-        delimiters.add(',');
-        delimiters.add('\n');
+        ArrayList<Character> delimiters = new ArrayList<>(2);
+        LinkedList<Integer> negative_numbers = new LinkedList<>();
         if (numbers.length() != 0)
         {
+            int num;
             if (numbers.startsWith("//"))
             {
-                delimiters.clear();
                 delimiters.add(numbers.charAt(2));
                 i = 4;
+            }
+            else
+            {
+                delimiters.add(',');
+                delimiters.add('\n');
             }
             for (; i < numbers.length(); i++)
             {
                 if (delimiters.contains(numbers.charAt(i)))
                 {
-                    sum += Integer.parseInt(number.toString());
+                    num = Integer.parseInt(number.toString());
+                    sum += num;
+                    if (num < 0) negative_numbers.add(num);
                     number.delete(0, number.length());
                 }
                 else number.append(numbers.charAt(i));
             }
-            sum += Integer.parseInt(number.toString());
+            num = Integer.parseInt(number.toString());
+            if (num < 0) negative_numbers.add(num);
+            sum += num;
+        }
+        if (negative_numbers.size() != 0)
+        {
+            String Error_Message = "Negatives not allowed: ";
+            for (Integer negatives: negative_numbers)
+            {
+                Error_Message = String.join(" ", Error_Message, negatives.toString());
+            }
+            throw new IllegalArgumentException(Error_Message);
         }
         return sum;
     }
