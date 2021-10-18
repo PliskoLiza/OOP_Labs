1e47018eec23050f9c7a7283869aa5064d1b9873
Plisko_Liza <pliskoliza@gmail.com>
2021-10-18 16:42:01 +0300

    First step

--- Lab2/src/main/java/oop/lab2/StringCalculator.java
+++ Lab2/src/main/java/oop/lab2/StringCalculator.java
@@ -5,6 +5,22 @@ public class StringCalculator
 {
     public static int Add(String numbers)
     {
-        return 0;
+        if(numbers.length() == 0) return 0;
+        else
+        {
+            int delimiters_count = 0;
+            int delimiter_place = 0;
+            for (int i = 0; i < numbers.length(); i++)
+            {
+                if (numbers.charAt(i) == ',') {delimiters_count ++; delimiter_place = i;}
+            }
+            if (delimiters_count != 0 && delimiter_place != 0)
+            {
+                int first = Integer.parseInt(numbers.substring(0, delimiter_place));
+                int second = Integer.parseInt(numbers.substring(delimiter_place + 1));
+                return first + second;
+            }
+            else return Integer.parseInt(numbers);
+        }
     }
 }
